43b61be00da1b21df1d52be38c72919fcb22f351
Byeongjin kim <loveuku04@gmail.com>
2021-10-12 16:34:05 -0400

    Emitted event-bus

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -10,17 +11,32 @@ const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res)=>{
   res.send(commentsByPostId[req.params.id] || []);
-  console.log(commentsByPostId[req.params.id]);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentID = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentID, content});
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentID,
+      content,
+      postId: req.params.id
+    }
+  });
+
   res.status(201).send(comments);
 });
+
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  res.send({});
+})
 
 app.listen(4001, ()=>{
   console.log('Listening on 4001');
