7a2363ce67413b8004b62c628168105b88caa375
Byeongjin kim <loveuku04@gmail.com>
2021-10-13 00:07:01 -0400

    add moderation service for comment

--- comments/index.js
+++ comments/index.js
@@ -17,7 +17,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   const commentID = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentID, content});
+  comments.push({ id: commentID, content, status: 'pending'});
   commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
@@ -25,15 +25,38 @@ app.post('/posts/:id/comments', async (req, res) => {
     data: {
       id: commentID,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   });
 
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Event Received:', req.body.type);
+
+  const {type, data} = req.body;
+
+  if (type === 'CommentModerated'){
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    })
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content
+      }
+    })
+  }
 
   res.send({});
 })
