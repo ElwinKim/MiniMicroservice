a383cc896afdfc5c1387690b4083f5aca6367681
Byeongjin kim <loveuku04@gmail.com>
2021-10-13 18:18:37 -0400

    LastCommit

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,9 +5,13 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
   
+  events.push(event);
+
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
   axios.post('http://localhost:4002/events', event);
@@ -16,6 +20,10 @@ app.post('/events', (req, res) => {
   res.send({ status: 'OK'});
 
 });
+
+app.get('/events', (req, res) => {
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log('Listening on 4005');
